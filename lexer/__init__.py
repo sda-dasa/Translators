ecbb373d86de50e2afc4582e82329bd0373c9beb
sda-dasa <saltykovadasa15@gmail.com>
2026-05-12 22:59:37 +0300

    Added __init__.py

---
+++ lexer/__init__.py
@@ -0,0 +1,5 @@
+from lexer.token_types import Token, TokenType, IdentifierInfo
+from lexer.scanner import Scanner
+from lexer.tables import KEYWORDS, OPERATORS, DELIMITERS
+
+__all__ = ['Token', 'TokenType', 'IdentifierInfo', 'Scanner', 'KEYWORDS', 'OPERATORS', 'DELIMITERS']
